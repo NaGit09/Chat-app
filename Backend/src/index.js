fd046779c6f972bbce18d0de6f39056ce6ff694e
NaGit09 <nhutanhtran806@gmail.com>
2025-03-31 23:58:30 +0700

    last update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,9 +6,10 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import cors from "cors";
 import {io ,server , app } from './lib/socket.js'
+import path from 'path'
 // config
 dotenv.config();
-
+const __dirname = path.resolve()
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookiesParser());
@@ -21,6 +22,15 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 // listen port 5001 cd B
 server.listen(PORT, () => {
   console.log(`SERVER IS RUNNING IN PORT ${PORT}`);
